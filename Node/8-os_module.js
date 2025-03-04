76893d940e34e3291167c123e57811ca0746c4da
CouchPtato <singhsomay467@gmail.com>
2025-03-04 12:39:51 +0530

    os module

---
+++ Node/8-os_module.js
@@ -0,0 +1,19 @@
+//Built-in modules: os, path, fs, http
+
+const os = require('os')
+
+// info about user 
+const user = os.userInfo()
+console.log(user);
+
+// returns system uptime
+console.log(`the system uptime is ${os.uptime()} seconds`)
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalmem: os.totalmem(),
+    freemem: os.freemem(), 
+}
+
+console.log(currentOS)
